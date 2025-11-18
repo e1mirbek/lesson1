58716d3960849e86a2c81c4f88fa1fdfe1ac9a2f
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-18 22:30:55 +0600

    практика с данными и с Grid View (тернарный оператор

--- lib/lessons/lesson01.dart
+++ lib/lessons/lesson01.dart
@@ -1,9 +1,28 @@
 
 
-
+// практика с Grid View и тернарный оператором
 
 
 import 'package:flutter/material.dart';
+import 'package:lesson1/widgets/example_container_widget.dart';
+
+
+
+
+List <String> texts = [
+  "Text 1",
+  "Text 2",
+  "Text 3",
+  "Text 4",
+  "Text 5",
+  "Text 6",
+  "Text 7",
+  "Text 8",
+  "Text 9",
+  "Text 10",
+];
+
+
 
 class Lesson01 extends StatefulWidget {
   const Lesson01({super.key});
@@ -14,35 +33,35 @@ class Lesson01 extends StatefulWidget {
 
 class _Lesson01State extends State<Lesson01> {
 
-  int selectedIndex = -1;
+  int selectedIndex = 0;
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Практика flutter"),
+        title: Text("Практика Grid View"),
+        centerTitle: true,
       ),
       body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: GridView.builder(
-              itemCount: 5,
+              itemCount: 10,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
                 mainAxisSpacing: 10,
                   crossAxisSpacing: 10
                 ), itemBuilder: (context, index) {
-              return InkWell(
+              return GestureDetector(
                 onTap: () {
                   setState(() {
                     selectedIndex = index;
                   });
                 },
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: selectedIndex == index ? Colors.red : Colors.blue,
+                child:
+                ExampleContainerWidget(
+                    color: selectedIndex == index ? Colors.red : Colors.blue, text: texts[index],
                 ),
               );
             }
