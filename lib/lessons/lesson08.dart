22b0adb266f9285370f4f427a43eca5d2b7a1551
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-21 13:59:31 +0600

    практика с ElevatedButton

--- lib/lessons/lesson08.dart
+++ lib/lessons/lesson08.dart
@@ -4,6 +4,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:lesson1/widgets/elevated_button_accout_widget.dart';
 
 class Lesson08 extends StatelessWidget {
   const Lesson08({super.key});
@@ -17,8 +18,9 @@ class Lesson08 extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -65,6 +67,59 @@ class Lesson08 extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 47.07,),
+      Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Total", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+              Row(
+                children: [
+                  Image.asset("assets/images/n.png", width: 20, height: 16,),
+                  const SizedBox(width: 5,),
+                  Text("60,000", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),),
+                ],
+              ),
+            ],
+          ),
+          Spacer(),
+          ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(199, 56),
+                    backgroundColor: Color(0xFFFFA451),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                  ),
+                  child: Center(
+                    child: Text("Checkout", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color(0xFFFFFFFF)),),)),
+        ],
+      ),
+            const SizedBox(height: 121,),
+           Row(
+             children: [
+               ElevatedButtonAccoutWidget(imagePath: "assets/images/google.png"),
+               ElevatedButtonAccoutWidget(imagePath: "assets/images/apple.png"),
+               ElevatedButtonAccoutWidget(imagePath: "assets/images/facebook.png"),
+             ],
+           ),
+            const SizedBox(height: 53,),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(169, 36),
+                    backgroundColor: Color(0xFF00D09E),
+                  ),
+                  child: Center(
+                    child: Text("Add Expenses", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
                 ),
         ),
