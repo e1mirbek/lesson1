b9cadc741c410e11836a0f51deb9831cf46c46e5
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-12-03 20:27:00 +0600

    ностройка навигаций

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -6,10 +6,15 @@ import 'package:lesson1/lessons/lesson04.dart';
 import 'package:lesson1/lessons/lesson05.dart';
 import 'package:lesson1/lessons/lesson06.dart';
 import 'package:lesson1/lessons/lesson07.dart';
+import 'package:lesson1/lessons/lesson08.dart';
+import 'package:lesson1/lessons/lesson09.dart';
+import 'package:lesson1/lessons/lesson10.dart';
+import 'package:lesson1/lessons/lesson11.dart';
+import 'package:lesson1/lessons/lesson12.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const Lesson03()),
+    GoRoute(path: '/', builder: (context, state) => const Lesson12()),
     GoRoute(path: '/second', builder: (context, state) => const Lesson06()),
     GoRoute(path: '/third', builder: (context, state) => const Lesson07()),
     GoRoute(path: '/fourth', builder: (context, state) => const Lesson05()),
